904b55af089b03a58bb9d91f077fe169da865d0c
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2024-07-21 22:15:42 +0200

    feat(security): add basic auth
    - install lexik/jwt-authentication-bundle 3.1
    - install symfony/uid 6.4.8
    - add ApiUser entity for auth
    - remove Greeting entity

---
+++ api/src/Entity/ApiUser.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * Represent a user of this API, mostly another application.
+ */
+#[Entity]
+class ApiUser implements UserInterface
+{
+    #[Id]
+    #[Column(type: UuidType::NAME, unique: true)]
+    private Uuid $id;
+
+    #[Column(type: Types::STRING, unique: true)]
+    private string $identifier;
+
+    #[Column(type: Types::STRING)]
+    private string $password;
+
+    #[Column(type: Types::DATETIME_IMMUTABLE)]
+    private \DateTimeImmutable $createdAt;
+
+    #[Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
+    private ?\DateTimeImmutable $updatedAt;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v7();
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = null;
+    }
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): ApiUser
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function setUserIdentifier(string $userIdentifier): ApiUser
+    {
+        $this->identifier = $userIdentifier;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): ApiUser
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): ApiUser
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): ApiUser
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // For any temporary, sensitive data on the user, clear it here
+    }
+}
